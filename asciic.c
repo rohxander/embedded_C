a25df0b8a826f32461cb4bfc17d54b300399efe2
Rohit Prakash <rohitmanjappallil@gmail.com>
2020-07-10 18:58:56 +0530

    added sciic.c

---
+++ asciic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	char c1,c2,c3,c4,c5,c6;
+	printf("Enter 6characters : ");
+	scanf("%c %c %c %c %c %c",&c1,&c2,&c3,&c4,&c5,&c6);
+	printf("\nASCII Code : %u,%u,%u,%u,%u,%u",c1,c2,c3,c4,c5,c6);
+	// printf("\nPress Enter Key to Exit Application \n");
+	// while(getchar() !='\n')
+	// {
+
+	// }
+	// getchar();
+}
